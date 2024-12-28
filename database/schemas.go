28713283177e480f0a61e5af022b9fa1fbb90f8d
SAGNIK PAL <palsagnik2102@gmail.com>
2024-12-28 20:48:37 +0530

    feat(db): Create db tables

--- database/schemas.go
+++ database/schemas.go
@@ -11,15 +11,15 @@ func createTables(db *sql.DB) error {
 	
 	// Create videos table
 	videoTable := `
-	CREATE TABLE IF NOT EXISTS videos (
-    video_id VARCHAR(50) UNIQUE PRIMARY KEY,
-    title TEXT NOT NULL,
-    description TEXT,
-    published_at TIMESTAMP NOT NULL,
-    channel_title VARCHAR(255) NOT NULL,
-    thumbnail_url TEXT,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-	);`
+		CREATE TABLE IF NOT EXISTS videos (
+		video_id VARCHAR(50) UNIQUE PRIMARY KEY,
+		title TEXT NOT NULL,
+		description TEXT,
+		published_at TIMESTAMP NOT NULL,
+		channel_title VARCHAR(255) NOT NULL,
+		thumbnail_url TEXT,
+		created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+		);`
 
 	_, err := db.Exec(videoTable)
 	if err != nil {
