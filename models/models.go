e2439393c5abd8b6db143c45a91fc949c5d9fd1f
SAGNIK PAL <palsagnik2102@gmail.com>
2024-12-28 21:21:50 +0530

    feat(api): Add struct for api responses

--- models/models.go
+++ models/models.go
@@ -9,3 +9,10 @@ type VideoItem struct {
 	ChannelTitle string `json:"channeltitle"`
 	ThumbnailURL string `json:"thumbnailurl"`
 }
+
+// api response model
+type VideoResponse struct {
+	Status  string      `json:"status"`
+	Data    []VideoItem `json:"data,omitempty"`
+	Message string      `json:"message,omitempty"`
+}
